2025433d4752c57c3de04fb6ceea6cdf6617f9cd
rib-sig599 <60521606+rib-sig599@users.noreply.github.com>
2023-05-18 18:55:46 -0400

    working nav bar
    implementing several html files

--- views.py
+++ views.py
@@ -6,3 +6,15 @@ views = Blueprint(__name__, "views")
 def home():
     return render_template("index.html")
 
+@views.route("/pricing")
+def pricing():
+    return render_template("pricing.html")
+
+
+@views.route("/information")
+def information():
+    return render_template("information.html")
+
+@views.route("/times")
+def times():
+    return render_template("times.html")
