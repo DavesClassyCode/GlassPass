f1575caa9ec16e1677c18c61c090ca8e881ce0a0
DillonM0812 <64936065+DillonM0812@users.noreply.github.com>
2023-05-22 14:13:04 -0400

    Preliminary unit testing work for SQL login connector

--- DBUserHandler.py
+++ DBUserHandler.py
@@ -4,7 +4,7 @@ Database interaction tool
 Created by Dillon M
 Created 5/20/2023
 Last Edited by Dillon M
-Last Modified  5/20/2023
+Last Modified  5/22/2023
 Created for CSI2999 Polyrhythm Skate semester project
 
 Relevant online documentation:
@@ -14,12 +14,18 @@ https://docs.python.org/3/library/sqlite3.html#module-sqlite3
 Change Log:
 5/20/2023: Initial Version
 
-To Do List:
+5/22/2023: Modified exception classes to avoid using general "Exception" class
+Work on preliminary unit testing
+
+Future Task List:
 Create function to add dummy user data
+Do unit testing
 """
 
 import os
 import sqlite3
+import unittest
+
 from passlib.hash import pbkdf2_sha256
 
 tableColumnDict = {'Users': '("UID" INTEGER NOT NULL UNIQUE, "FirstName" TEXT, "LastName" TEXT, "Email" TEXT, "Password" TEXT, PRIMARY KEY ("UID" AUTOINCREMENT))'}
@@ -27,11 +33,13 @@ tableInsertDict = {'Users': '("UID", "FirstName", "LastName", "Email", "Password
 
 class DBHandler():
 
-    def __init__(self):
+    def __init__(self,databasePath = None):
         """
         Initializes the SQLite Database Connection, raises FileNotFoundError if the database is missing
         """
-        self.dbPath = 'SkateDB.db'
+        self.dbPath = databasePath
+        if self.dbPath is None:
+            self.dbPath = 'SkateDB.db'
         if not os.path.isfile(self.dbPath):
             raise FileNotFoundError("Database file not found in base program directory")
         self.dbConnection = sqlite3.connect(self.dbPath)
@@ -56,22 +64,35 @@ class DBHandler():
         :return:
         """
         FirstName, LastName, Email, Password = tuple(args)
-        self.checkDuplicateUserInfo(FirstName, LastName, Email)
+        try:
+            self.checkDuplicateUserInfo(FirstName, LastName, Email)
+        except sqlite3.DataError as e:
+            print(e)
+            return False
         passHash = self.hashPassword(str(Password))
         statement = 'INSERT INTO Users (FirstName, LastName, Email, Password) VALUES (?, ?, ?, ?);'
         arg = (str(FirstName), str(LastName), str(Email), passHash)
         self.dbCursor.execute(statement, arg)
         self.dbConnection.commit()
+        return True
 
     def checkDuplicateUserInfo(self, FirstName, LastName, Email):
+        """
+        Verify that the database does not have a matching first/last name pair as a new user
+        and that a new email does not already exist in the database
+        :param FirstName:
+        :param LastName:
+        :param Email:
+        :return:
+        """
         statement = 'SELECT FirstName, LastName FROM Users WHERE FirstName=? AND LastName = ?;'
         arg = (FirstName, LastName,)
-        if self.dbCursor.execute(statement, arg).fetchone()[0] is not None:
-            raise DataError('Duplicate First and Last name pair')
+        if len(self.dbCursor.execute(statement, arg).fetchall()) > 0:
+            raise sqlite3.DataError('Duplicate First and Last name pair')
         statement = 'SELECT Email FROM Users WHERE Email=?;'
         arg = (Email,)
-        if self.dbCursor.execute(statement, arg).fetchone()[0] is not None:
-            raise DataError('Duplicate Email found in table')
+        if self.dbCursor.execute(statement, arg).fetchone() is not None:
+            raise sqlite3.DataError('Duplicate Email found in table')
 
     def retrievePassHash(self, UID=None, FirstName=None, LastName=None, Email=None):
         """
@@ -132,3 +153,34 @@ class DBHandler():
         if self.dbCursor.execute(statement,arg).fetchone()[0] is None:
             return False
         return self.verifyPassword(passwordArg, Email=emailArg)
+
+class DBUserHandlerUnitTesting(unittest.TestCase):
+    def testBasicNewUserInsert(self):
+        testDB = DBHandler('unittestDB.db')
+        testDB.dbCursor.execute('DELETE FROM Users;')
+        testDB.dbConnection.commit()
+        unittest.TestCase.assertTrue(self, expr=testDB.insertNewUserData(['testFirstName1', 'testLastName1', 'test1@mail.com', 'password1']), msg='Failed to add first user')
+        unittest.TestCase.assertTrue(self, expr=testDB.insertNewUserData(['testFirstName2', 'testLastName2', 'test2@mail.com', 'password2']), msg='Failed to add second user')
+        unittest.TestCase.assertTrue(self, expr=testDB.insertNewUserData(['testFirstName3', 'testLastName3', 'test3@mail.com', 'password2']), msg='Failed to add user with duplicate password')
+        unittest.TestCase.assertTrue(self, expr=testDB.insertNewUserData(['testFirstName2', 'testLastName3', 'test4@mail.com', 'password3']), msg='Failed to add user with mismatched repeat names')
+        testDB.dbCursor.execute('DELETE FROM Users;')
+        testDB.dbConnection.commit()
+
+    def testDuplicateUserInsert(self):
+        testDB = DBHandler('unittestDB.db')
+        testDB.dbCursor.execute('DELETE FROM Users;')
+        testDB.insertNewUserData(['testFirstName1', 'testLastName1', 'test1@mail.com', 'password1'])
+        unittest.TestCase.assertFalse(self, expr=testDB.insertNewUserData(['testFirstName1', 'testLastName1', 'test2@mail.com', 'password2']),msg='Did not add duplicate user firstname/lastname')
+        unittest.TestCase.assertFalse(self, expr=testDB.insertNewUserData(['testFirstName2', 'testLastName2', 'test1@mail.com', 'password3']),msg='Did not add duplicate user email')
+        testDB.dbCursor.execute('DELETE FROM Users;')
+        testDB.dbConnection.commit()
+
+    def testGoodLogin(self):
+        pass
+
+    def testBadLogin(self):
+        pass
+
+if __name__ == '__main__':
+    #Only for unit testing database connector
+    unittest.main()
